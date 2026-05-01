839844973775d6f837a96c4f1575eb8b739ce5b8
Manuel Domínguez Dorado <manuel.dominguez@enzinatec.com>
2026-05-01 21:23:35 +0200

    Changing artifacts name

--- src/main/java/com/manolodominguez/fleco/algorithm/ChromosomeComparator.java
+++ src/main/java/com/manolodominguez/fleco/algorithm/ChromosomeComparator.java
@@ -1,41 +1,41 @@
 /* 
-     *******************************************************************************
-     * CyberTOMP® is a cybersecurity framework that helps organizations manage and 
-     * assess security at a tactical and operational level by focusing on business 
-     * assets. It provides structured processes, metrics, and roles to align people,
-     * technology, and supply chains, enabling informed decision-making based on 
-     * asset criticality. Compatible with standards like ISO 27001 and NIST, it also
-     * incorporates practical tools and optimization techniques to deliver a 
-     * cohesive, measurable, and efficient approach to cybersecurity.
-     * 
-     * Within CyberTOMP®, FLECO (Fast, Lightweight, and Efficient Cybersecurity 
-     * Optimization) is an adaptive, constrained genetic algorithm designed to 
-     * support asset cybersecurity teams in decision-making throughout the 
-     * application of the CyberTOMP® framework. The cybertomp-fleco-library provides
-     * FLECO as a dynamic Java library.
-     *
-     * Visit https://cybertomp.org to learn more about the CyberTOMP® framework, 
-     * the collaborative project behind it, its components, and its research 
-     * foundations, all of which continuously evolve based on empirical evidence and
-     * solid, verifiable principles.
-     *
-     *******************************************************************************
-     * Copyright (C) Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com.
-     * 
-     * This program is free software: you can redistribute it and/or modify it under 
-     * the terms of the GNU Lesser General Public License as published by the Free 
-     * Software Foundation, either version 3 of the License, or (at your option) any
-     * later version.
-     * 
-     * This program is distributed in the hope that it will be useful, but WITHOUT 
-     * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-     * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
-     * details.
-     * 
-     * You should have received a copy of the GNU Lesser General Public License 
-     * along with this program. If not, see 
-     * https://www.gnu.org/licenses/lgpl-3.0.en.html.
-     *******************************************************************************
+ *******************************************************************************
+ * CyberTOMP® is a cybersecurity framework that helps organizations manage and 
+ * assess security at a tactical and operational level by focusing on business 
+ * assets. It provides structured processes, metrics, and roles to align people,
+ * technology, and supply chains, enabling informed decision-making based on 
+ * asset criticality. Compatible with standards like ISO 27001 and NIST, it also
+ * incorporates practical tools and optimization techniques to deliver a 
+ * cohesive, measurable, and efficient approach to cybersecurity.
+ * 
+ * Within CyberTOMP®, FLECO (Fast, Lightweight, and Efficient Cybersecurity 
+ * Optimization) is an adaptive, constrained genetic algorithm designed to 
+ * support asset cybersecurity teams in decision-making throughout the 
+ * application of the CyberTOMP® framework. The cybertomp-fleco-library provides
+ *  FLECO as a dynamic Java library.
+ *
+ * Visit https://cybertomp.org to learn more about the CyberTOMP® framework, 
+ * the collaborative project behind it, its components, and its research 
+ * foundations, all of which continuously evolve based on empirical evidence and
+ * solid, verifiable principles.
+ *
+ *******************************************************************************
+ * Copyright (C) Manuel Domínguez Dorado - ingeniero@ManoloDominguez.com.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU Lesser General Public License as published by the Free 
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this program. If not, see 
+ * https://www.gnu.org/licenses/lgpl-3.0.en.html.
+ *******************************************************************************
  */
 package com.manolodominguez.fleco.algorithm;
 
